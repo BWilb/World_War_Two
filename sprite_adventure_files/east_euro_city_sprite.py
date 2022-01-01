2bbe4b16ddb92e90d7ddb5e8c420a04200271ebb
Bryce Wilbur <bryce.wilbur@my.simpson.edu>
2022-01-01 13:13:11 -0600

    display score, health, and germans on bottom of sprite game

--- sprite_adventure_files/east_euro_city_sprite.py
+++ sprite_adventure_files/east_euro_city_sprite.py
@@ -1,8 +1,108 @@
 import text_adventure_files
+import adolf_sprite_ai
 import arcade
 SPRITE_SCALING = 0.5
 
 class Europa:
-    def __init__(self, adolf):
+    def __init__(self, north, north_east, east, south_east, south, south_west, west, north_west):
+        """Creation of variables that will be constraints upon sprite movement"""
+        self.north = north
+        self.north_east = north_east
+        self.east = east
+        self.south_east = south_east
+        self.south = south
+        self.south_west = south_west
+        self.west = west
+        self.north_west = north_west
         self.background = None
-        self.germans = []
+        self.germans = None
+
+def city_one():
+    moscow = Europa(None, None, None, None, None, None, None, None)
+    moscow.germans = arcade.SpriteList()
+
+    return moscow
+
+def city_two():
+    stalingrad = Europa(None, None, None, None, None, None, None, None)
+    stalingrad.germans = arcade.SpriteList()
+
+    return stalingrad
+
+def city_three():
+    kiev = Europa(None, None, None, None, None, None, None, None)
+    kiev.germans = arcade.SpriteList()
+
+    return kiev
+
+def city_four():
+    leningrad = Europa(None, None, None, None, None, None, None, None)
+    leningrad.germans = arcade.SpriteList()
+
+    return leningrad
+
+def city_five():
+    brest_litvosk = Europa(None, None, None, None, None, None, None, None)
+    brest_litvosk.germans = arcade.SpriteList()
+
+    return brest_litvosk
+
+def city_six():
+    riga = Europa(None, None, None, None, None, None, None, None)
+    riga.germans = arcade.SpriteList()
+
+    return riga
+
+def city_seven():
+    warsaw = Europa(None, None, None, None, None, None, None, None)
+    warsaw.germans = arcade.SpriteList()
+
+    return warsaw
+
+def city_eight():
+    konigsberg = Europa(None, None, None, None, None, None, None, None)
+    konigsberg.germans = arcade.SpriteList()
+
+    return konigsberg
+
+def city_nine():
+    kursk = Europa(None, None, None, None, None, None, None, None)
+    kursk.germans = arcade.SpriteList()
+
+    return kursk
+
+def city_ten():
+    breslau = Europa(None, None, None, None, None, None, None, None)
+    breslau.germans = arcade.SpriteList()
+
+    return breslau
+
+def city_eleven():
+    bucharest = Europa(None, None, None, None, None, None, None, None)
+    bucharest.germans = arcade.SpriteList()
+
+    return bucharest
+
+def city_twelve():
+    budapest = Europa(None, None, None, None, None, None, None, None)
+    budapest.germans = arcade.SpriteList()
+
+    return budapest
+
+def city_thirteen():
+    prague = Europa(None, None, None, None, None, None, None, None)
+    prague.germans = arcade.SpriteList()
+
+    return prague
+
+def city_fourteen():
+    stettin = Europa(None, None, None, None, None, None, None, None)
+    stettin.germans = arcade.SpriteList()
+
+    return stettin
+
+def city_fifteen():
+    berlin = Europa(None, None, None, None, None, None, None, None)
+    berlin.germans = arcade.SpriteList()
+
+    return berlin
--- sprite_adventure_files/euro_sprite_front.py
+++ sprite_adventure_files/euro_sprite_front.py
@@ -1,20 +1,21 @@
 import text_adventure_files.adolf_hitler
+import adolf_sprite_ai
 import arcade
 import east_euro_city_sprite
 import random
 
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 800
-SPRITE_SCALING = 0.25
+SCREEN_WIDTH = 1500
+SCREEN_HEIGHT = 850
+SPRITE_SCALING = 0.5
 
 """Movement variable is open-ended just so that user can go their own speed"""
-"""question = False
+question = False
 while not question:
     try:
         MOVEMENT_SPEED = int(input("How fast would you like to move?: "))
         question = True
     except:
-        print("You are supposed to input a Number!!!!!")"""
+        print("You are supposed to input a Number!!!!!")
 
 class GeorgyZhukov(arcade.Sprite):
     def __init__(self, file, sprite_scaling):
@@ -31,22 +32,22 @@ class WorldWarTwo(arcade.Window):
     def __init__(self, SCREEN_WIDTH, SCREEN_HEIGHT):
         """initialization method"""
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "World War Two")
-        """self.zhukov = None
-        #self.hitler = text_adventure_files.adolf_hitler.AdolfHitler()
+        self.zhukov = None
+        self.hitler = adolf_sprite_ai.AdolfHitler("zombie_idle.png", SPRITE_SCALING)
         #self.city = east_euro_city_sprite.Europa()
-        self.player_list = None
-        self.physics_engine = None"""
-
-    """def setup(self):
         self.player_list = arcade.SpriteList()
-        self.zhukov = GeorgyZhukov("marshal-zhukov.png", SPRITE_SCALING)
+        self.physics_engine = None
+
+    def setup(self):
+        self.player_list = arcade.SpriteList()
+        self.zhukov = GeorgyZhukov("georgy_zhukov.png", SPRITE_SCALING)
         self.zhukov.center_x = SCREEN_WIDTH // 2
         self.zhukov.center_y = SCREEN_HEIGHT // 2
-        self.player_list.append(self.zhukov)"""
+        self.player_list.append(self.zhukov)
 
-    """Create individual cities for city object"""
-    #self.physics_engine = arcade.PhysicsEngineSimple(self.zhukov)
-    """add city ground list to second parameter of physics engine"""
+        """Create individual cities for city object"""
+        #self.physics_engine = arcade.PhysicsEngineSimple(self.zhukov)
+        """add city ground list to second parameter of physics engine"""
 
     def on_draw(self):
         """Drawing method"""
@@ -58,23 +59,42 @@ class WorldWarTwo(arcade.Window):
                                           0,
                                           arcade.csscolor.GOLD)
 
-
         """Start drawing the cities and their backgrounds here"""
-        #self.player_list.draw()
+        self.player_list.draw()
+        arcade.draw_lrtb_rectangle_filled(0, SCREEN_WIDTH, 200, 0, arcade.csscolor.BLACK)
+        arcade.draw_text(f"Zhukov Score: {self.zhukov.scorecard}", 25, 100, arcade.csscolor.RED)
+        arcade.draw_text(f"Zhukov health: {self.zhukov.health}", 25, 150, arcade.csscolor.GOLD)
+        arcade.draw_text(f"Remaining germans: {self.hitler.german_list}", 725, 100, arcade.csscolor.GOLD)
+        arcade.draw_text(f"Hitler's score: {self.hitler.score}", 1325, 100, arcade.csscolor.RED)
+        arcade.draw_text(f"Hitler's health: {self.hitler.health}", 1325, 150, arcade.csscolor.WHITE)
 
-"""def on_update(self, delta_time: float):
+    def on_update(self, delta_time: float):
         self.player_list.update()
-        self.zhukov.update()
         #self.hitler.update()
-        add the logic for any scoring for both sides"""
+        """add the logic for any scoring for both sides"""
 
+    def on_key_press(self, key: int, modifiers: int):
+        if key == arcade.key.RIGHT:
+            self.zhukov.change_x = MOVEMENT_SPEED
+        elif key == arcade.key.LEFT:
+            self.zhukov.change_x = -MOVEMENT_SPEED
+        elif key == arcade.key.UP:
+            self.zhukov.change_y = MOVEMENT_SPEED
+        elif key == arcade.key.DOWN:
+            self.zhukov.change_y = -MOVEMENT_SPEED
+
+    def on_key_release(self, key: int, modifiers: int):
+        if key == arcade.key.RIGHT or key == arcade.key.LEFT:
+            self.zhukov.change_x = 0
+        elif key == arcade.key.UP or key == arcade.key.DOWN:
+            self.zhukov.change_y = 0
 
 def main():
     """main method
     placed outside of game's scope, so it can call to game
     """
     game = WorldWarTwo(SCREEN_WIDTH, SCREEN_HEIGHT)
-    game.on_draw()
+    game.setup()
     arcade.run()
 
 if __name__ == "__main__":
